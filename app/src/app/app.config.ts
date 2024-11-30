567d6bc1ead1f582c4f8752eaf629d93f12c178c
psstoykov <145485240+psstoykov@users.noreply.github.com>
2024-11-30 18:03:19 +0100

    updated firebase

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -4,10 +4,58 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getDatabase, ref, set } from 'firebase/database';
+import {
+  initializeApp as initializeApp_alias,
+  provideFirebaseApp,
+} from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getDatabase as getDatabase_alias,
+  provideDatabase,
+} from '@angular/fire/database';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyC5QOr-3qAgg2Ofpum5I5soddugAFV6owM',
+//   authDomain: 'angular-77bd6.firebaseapp.com',
+//   databaseURL:
+//     'https://angular-77bd6-default-rtdb.europe-west1.firebasedatabase.app',
+//   projectId: 'angular-77bd6',
+//   storageBucket: 'angular-77bd6.firebasestorage.app',
+//   messagingSenderId: '959123590684',
+//   appId: '1:959123590684:web:0d8288cf2c5fe0c95e99d9',
+//   measurementId: 'G-F8MX3NVC2N',
+// };
+
+// Initialize Firebase
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'angular-77bd6',
+        appId: '1:959123590684:web:0d8288cf2c5fe0c95e99d9',
+        databaseURL:
+          'https://angular-77bd6-default-rtdb.europe-west1.firebasedatabase.app',
+        storageBucket: 'angular-77bd6.firebasestorage.app',
+        apiKey: 'AIzaSyC5QOr-3qAgg2Ofpum5I5soddugAFV6owM',
+        authDomain: 'angular-77bd6.firebaseapp.com',
+        messagingSenderId: '959123590684',
+        measurementId: 'G-F8MX3NVC2N',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
   ],
 };
--- app/src/app/user/login/login.component.ts
+++ app/src/app/user/login/login.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
 import { NgForm, FormsModule } from '@angular/forms';
+import { Database, get, ref, list, child } from '@angular/fire/database';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +16,38 @@ import { NgForm, FormsModule } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private db: Database,
+    private auth: Auth
+  ) {
+    //Sign in new user
+    // createUserWithEmailAndPassword(auth, 'niki@abv.bg', '123123').then(
+    //   (userCredential) => {
+    //     // Signed up
+    //     const user = userCredential.user;
+    //     // ...
+    //     console.log(user);
+    //   }
+    // );
+    //get database ref
+    // const dbRef = ref(this.db);
+    //read from database
+    // get(child(dbRef, 'Users')).then((info) => console.log(info.val()));
+
+    signInWithEmailAndPassword(auth, 'nikii@abv.bg', '123123')
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error.message);
+      });
+  }
 
   login(form: NgForm) {
     const { email, password } = form.value;
