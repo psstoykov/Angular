fabbddeb5e3e4623e757abeb02bfb8f183422bb3
psstoykov <145485240+psstoykov@users.noreply.github.com>
2024-12-06 01:37:44 +0100

    search implemented

--- app/src/app/gallery/gallery.component.ts
+++ app/src/app/gallery/gallery.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, input, OnInit, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  input,
+  OnInit,
+  Signal,
+  signal,
+} from '@angular/core';
 import { ApiService } from '../api.service';
 import { Post } from '../types/posts';
 import { RouterLink } from '@angular/router';
@@ -14,14 +21,22 @@ import { FormsModule } from '@angular/forms';
 })
 export class GalleryComponent implements OnInit {
   posts = signal<Post[]>([]);
-  search = signal<Post[]>([]);
+  result: Post[] = [];
+  sq = signal<string>('');
+
   postId = input.required<string>(); //input signal for dynamic routing >>>postId is url param
 
   constructor(private apiService: ApiService, private firestore: Auth) {}
-
   searchFunc(event: Event) {
-    const sq = (event.target as HTMLInputElement).value;
-    //filter here
+    this.sq.set((event.target as HTMLInputElement).value);
+    //create filtered array
+    this.result = this.posts().filter(
+      (post) =>
+        post.title.includes(this.sq()) ||
+        post.description.includes(this.sq()) ||
+        post.ownerUsername.includes(this.sq())
+    );
+    console.log(this.sq());
   }
   ngOnInit() {
     const obs = this.apiService.getAllPosts();
@@ -32,6 +47,7 @@ export class GalleryComponent implements OnInit {
         data.id = doc.id;
 
         this.posts.update((prevPosts) => [...prevPosts, data]);
+        this.result = this.posts();
       })
     );
   }
